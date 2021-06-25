b8fd6e36025c031484877bbfaf3b4ddecefe671d
Leon Z <leonz@Leons-MacBook-Pro.local>
2021-06-25 15:13:40 +0800

    first commit

---
+++ inverted_index/basic_template/function_template.cpp
@@ -0,0 +1,22 @@
+//template1.cpp
+#include <iostream>
+template <typename T>
+void swap(T &a, T &b)
+{
+    T tmp;
+    tmp = a;
+    a = b;
+    b = tmp;
+}
+int main(int argc, char *argv[])
+{
+    int a = 2;
+    int b = 3;
+    swap(a, b); // 使用函数模板
+    std::cout << "a=" << a << ", b=" << b << std::endl;
+    double c = 1.1;
+    double d = 2.2;
+    swap(c, d);
+    std::cout << "c=" << c << ", d=" << d << std::endl;
+    return 0;
+}
